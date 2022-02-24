a7109a7467a39a6a1f04d03a297cca27b39a6690
Daniel H <daniel.hdz.garcia@gmail.com>
2022-02-24 12:35:40 -0500

    Update analisis.js

--- docs/Estadistica/analisis.js
+++ docs/Estadistica/analisis.js
@@ -1,15 +1,4 @@
-const salariosCol = colombia.map(
-    function (personita) {
-        return personita.salary;
-    }
-);
-
-const salariosColSorted = salariosCol.sort (
- function (salaryA, salaryB) {
-    return salaryA - salaryB;
- }   
-);
-
+// Helpers
 function esPar (numerito) {
     return (numerito % 2 === 0);
 }
@@ -25,9 +14,9 @@ function calcularMediaAritmetica(lista) {
     const promedioLista = sumaLista / lista.length;
 
     return promedioLista;
-
 };
 
+// Calculadora de mediana
 function medianaSalarios(lista) {
     const mitad = parseInt(lista.length / 2);
 
@@ -39,9 +28,37 @@ function medianaSalarios(lista) {
         return mediana;
     } else {
         const personitaMitad = lista[mitad];
+        return personitaMitad
     }
 }
 
-console.log(
-    medianaSalarios(salariosColSorted)
+// Mediana General
+const salariosCol = colombia.map(
+    function (personita) {
+        return personita.salary;
+    }
 );
+
+const salariosColSorted = salariosCol.sort (
+ function (salaryA, salaryB) {
+    return salaryA - salaryB;
+ }   
+);
+const medianaGeneralCol = medianaSalarios(salariosColSorted)
+
+// Mediana del top 10%
+
+const spliceStart = (salariosColSorted.length * 90 / 100);
+const spliceCount = salariosColSorted.length - spliceStart;
+
+const salariosColTop10 = salariosColSorted.splice(
+    spliceStart,
+    spliceCount,
+);
+
+const medianaTop10Col = medianaSalarios(salariosColTop10);
+
+console.log({
+medianaGeneralCol, 
+medianaTop10Col,
+});
